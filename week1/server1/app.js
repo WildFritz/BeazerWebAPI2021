4211c1cc44ad61c02dc8c15853261d607ffa5917
WildFritz <maleekbeazer@gmail.com>
2021-01-28 14:45:10 -0500

    week 3 thursday

--- week1/server1/app.js
+++ week1/server1/app.js
@@ -1,1 +1,2 @@
 console.log("Hello world")
+//Set-ExecutionPolicy -Scope Process -ExecutionPolicy Bypass 
--- week3/app.js
+++ week3/app.js
@@ -10,23 +10,106 @@ app.get('/', function(req,res){
 
 app.use('/client', express.static(__dirname+'/client'))
 
+
+
+//Server side communication
 serv.listen(3000, function(){
     console.log('Connected on localhost 3000')
 })
 
-//Server side communication
+
+var socketList = {}
+var playerList = {}
+
+var Player = function(id){
+    var self = {
+        x:400,
+        y:300,
+        id: id,
+        number:  Math.floor(Math.random() * 10),
+        right:  false,
+        left:  false,
+        up:  false,
+        down:  false,
+        speed:  10
+    }
+    self.updatePosition = function(){
+        if(self.right)
+        self.x += self.speed
+        if(self.left)
+        self.x -= self.speed
+        if(self.up)
+        self.y -= self.speed
+        if(self.down)
+        self.y += self.speed
+    }
+    return self
+}
+
 io.sockets.on('connection', function(socket){
     console.log("Socket Connected")
 
-    socket.on('sendMsg', function(data){
-        console.log(data.message)
+    socket.id = Math.random()
+   // socket.x = 0
+   // socket.y = Math.floor(Math.random() * 600)
+   // socket.number = Math.floor(Math.random() * 10)
+
+    //add something to socketList
+    socketList[socket.id] = socket
+    
+
+    var player = new Player(socket.id)
+    playerList[socket.id] = player
+
+    //disconnection event
+    socket.on('disconnect',function(){
+        delete socketList[socket.id]
+        delete playerList[socket.id]
     })
 
-    socket.on('sendBtnMsg', function(data){
-        console.log(data.message)
+    //recieved player input
+    socket.on('keypress', function(data){
+        if (data.inputId === 'up')
+            player.up = data.state
+        if (data.inputId === 'down')
+            player.down = data.state
+        if (data.inputId === 'left')
+            player.left = data.state
+        if (data.inputId === 'right')
+            player.right = data.state
     })
 
-    socket.emit('messageFromServer', {
-        message: 'Hey Maleek do you like the way I WOO'
-    })
+
+    //old examples from wednesday
+    // socket.on('sendMsg', function(data){
+    //     console.log(data.message)
+    // })
+
+    // socket.on('sendBtnMsg', function(data){
+    //     console.log(data.message)
+    // })
+
+    // socket.emit('messageFromServer', {
+    //     message: 'Hey Maleek do you like the way I WOO'
+    // })
 })
+
+//setup update loop
+setInterval(function () {
+    var pack = []
+    
+    for (var i in playerList) {
+        var player = playerList[i]
+        player.updatePosition()
+       // console.log(player)
+        pack.push({
+            x: player.x,
+            y: player.y,
+            number:player.number
+        })
+    }
+    for (var i in socketList) {
+        var socket = socketList[i]
+        socket.emit('newPositions', pack)
+    }
+}, 1000 / 30)
